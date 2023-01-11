0bdbbe1b788743764ce6c7bf61faf7d998c0b250
fatma-eloueslati <80330424+fatma-eloueslati@users.noreply.github.com>
2023-01-11 23:49:33 +0100

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,14 @@
+
+variable "location" {
+  description = "the location of the vpc"
+  default     = "West Europe"
+}
+variable "username" {
+  description = "the username of user"
+  default     = "adminuser"
+}
+
+variable "password" {
+  description = "the password of user"
+  default     = "fatmaoueslati2mpssr2023"
+}
